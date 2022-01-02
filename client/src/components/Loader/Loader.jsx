5527f47453a01c1e8294a7a7991c638fc1450d24
Developer Junaid <qjunaid623@gmail.com>
2022-01-02 10:18:01 +0500

    completed welcome section styling

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,5 +1,9 @@
 const Loader = () => {
-  return <h1>Loader</h1>;
+  return (
+    <div className="flex justify-center items-center py-3">
+      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-red-700" />
+    </div>
+  );
 };
 
 export default Loader;
