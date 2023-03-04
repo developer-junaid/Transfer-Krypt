84b15ae30abcb8c7e81eeef22ee544b2db6e7a9b
Malhar-Soomro <malharsoomro2@gmail.com>
2023-03-04 12:45:51 +0500

    added flex-row property in the container to display elements in the column at the width of 768.

--- client/src/components/Services/Services.jsx
+++ client/src/components/Services/Services.jsx
@@ -22,7 +22,7 @@ const ServiceCard = ({ color, title, icon, subtitle }) => (
 
 const Services = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services">
+    <div className="flex flex-col mf:flex-row w-full justify-center items-center gradient-bg-services">
       <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
         <div className="flex-1 flex flex-col justify-start items-start">
           <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
